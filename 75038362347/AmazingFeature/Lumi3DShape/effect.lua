56ec59327cd26a3c9f7ed181889d8ef3ad740afc
Maki Nishi <tantamoraru49@gmail.com>
2026-05-02 09:33:20 +0200

    Add Lumi3DShape effect configuration

---
+++ 75038362347/AmazingFeature/Lumi3DShape/effect.lua
@@ -0,0 +1,20 @@
+return {
+tag = "AfterEffects",
+effect = "InShotEffect",
+parameters = {
+Lumi3DShape = {
+intensity = 1.35,
+depth = 0.78,
+scale = 1.12,
+rotationX = 12,
+rotationY = 28,
+rotationZ = 6,
+lightX = 0.62,
+lightY = 0.41,
+shadow = 0.33,
+highlight = 0.88,
+bevel = 0.22,
+smoothing = 0.9
+}
+}
+}
